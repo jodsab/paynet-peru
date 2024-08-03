6e858617461eb08bd361cf00e4f4c8674140a113
jodsab <camilo.b.q@hotmail.com>
2024-08-03 14:07:25 -0500

    feat: menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
+import { IoMdMenu } from "react-icons/io";
+import { IoCloseOutline } from "react-icons/io5";
 import "./styles.scss";
 
 const NavBar = () => {
@@ -9,36 +11,47 @@ const NavBar = () => {
 
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
-    console.log(openMenu);
+  };
+
+  const closeMenu = () => {
+    setOpenMenu(false);
   };
 
   return (
-    <div className="navbar_container">
-      <div className="navbar_header">
-        <h4>PAYNET</h4>
-        <button onClick={toggleMenu}>
-          <label className="burger">
-            <span></span>
-            <span></span>
-            <span></span>
-          </label>
-        </button>
-      </div>
+    <div className="navbar_wrapper">
+      <div className="navbar_container">
+        <div className="navbar_header margin">
+          <h4>PAYNET</h4>
+          <button className="toggle_menu_button" onClick={toggleMenu}>
+            {openMenu ? (
+              <IoCloseOutline size={35} color="#014b8e" />
+            ) : (
+              <IoMdMenu size={35} color="#014b8e" />
+            )}
+          </button>
+        </div>
 
-      {openMenu && (
-        <div className="menu_backdrop" onClick={() => setOpenMenu(false)}></div>
-      )}
-
-      <div className={`toggle_menu ${openMenu ? "open" : "close"}`}>
-        <button>Nosotros</button>
-        <button>Funcionamiento</button>
-        <button>Comisiones</button>
-        <button>Blog</button>
-        <div className="extra_menu">
-          <button>Iniciar sesión</button>
-          <button>Registrar</button>
+        <div className={`toggle_menu ${openMenu ? "open" : "close"}`}>
+          <button>
+            <p>Nosotros</p>
+          </button>
+          <button>
+            <p>Funcionamiento</p>
+          </button>
+          <button>
+            <p>Comisiones</p>
+          </button>
+          <button>
+            <p>Blog</p>
+          </button>
+          <div className="extra_menu">
+            <button className="iniciar_sesion">Iniciar sesión</button>
+            <button className="registrarse">Registrar</button>
+          </div>
         </div>
       </div>
+
+      {openMenu && <div className="backdrop" onClick={closeMenu}></div>}
     </div>
   );
 };
